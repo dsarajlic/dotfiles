63defa042450836d91c8cad2661d9371ea5d4498
Damir Sarajlic <damirdesigned@gmail.com>
2019-03-22 22:27:30 +1100

    chore: add bash-completion2 setup

--- brew.sh
+++ brew.sh
@@ -26,6 +26,11 @@ brew install bash-completion2
 brew install zsh
 brew install tree
 
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
+  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
+  chsh -s "${BREW_PREFIX}/bin/bash";
+fi;
 
 # To make tmux "set-option -g default-command "reattach-to-user-namespace -l
 # zsh" work
